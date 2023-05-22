26e4f25f696244e7ea09dc469874368f3d812c9c
sixbeeshades <abhijith.p.subash@gmail.com>
2023-05-23 00:02:57 +0530

    about fix

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,45 +1,78 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 const About = () => {
   return (
-    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-screen py-24 sm:py-32">
-      <p className="text-primary">
-        Welcome to the About section of our website! At [Your Company Name], we
-        are passionate about empowering developers and making their lives
-        easier. We understand the challenges that software engineers face in
-        their day-to-day work, and we strive to provide innovative tools that
-        enhance productivity and streamline the development process. Our team is
-        comprised of experienced software engineers who have firsthand knowledge
-        of the intricacies and demands of modern backend development. We have
-        spent countless hours designing and refining our flagship product, the
-        Noex Framework, to cater specifically to the needs of developers. The
-        Noex Framework is a powerful and versatile tool that allows developers
-        to build robust and scalable backend applications with ease. Whether you
-        are working on a small personal project or a large enterprise solution,
-        our framework provides the flexibility and functionality you need to
-        bring your ideas to life. One of the standout features of the Noex
-        Framework is its intuitive and developer-friendly nature. We believe
-        that coding should be a creative and enjoyable experience, and our
-        framework is designed to support that belief. With a clean and concise
-        syntax, coupled with comprehensive documentation and examples, you can
-        quickly dive into the world of Noex and start building amazing
-        applications in no time. In addition to the Noex Framework, we also
-        offer a powerful command-line interface (CLI) tool that further enhances
-        your development workflow. Our CLI tool provides a range of handy
-        commands and utilities, enabling you to scaffold projects, generate code
-        snippets, manage dependencies, and much more. It's the perfect companion
-        for developers who value efficiency and productivity. At [Your Company
-        Name], we are committed to providing exceptional support to our users.
-        We actively listen to our community and constantly iterate on our
-        products based on their feedback. We believe that collaboration and open
-        communication are the keys to building successful developer tools. We
-        invite you to join our growing community of developers and experience
-        the power of the Noex Framework and our CLI tool. Explore our website to
-        learn more about our offerings, access documentation, and get started on
-        your next project. We're excited to be a part of your journey and help
-        you build remarkable software. Happy coding! [Your Name] Founder and
-        Lead Developer at [Your Company Name]
-      </p>
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-screen ">
+      <section className="text-noexWhite body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-col">
+          <div className="lg:w-4/6 mx-auto">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img
+                alt="content"
+                className="object-cover object-center h-full w-full"
+                src="https://dummyimage.com/1200x500"
+              />
+            </div>
+            <div className="flex flex-col sm:flex-row mt-10">
+              <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-10 h-10"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <div className="flex flex-col items-center text-center justify-center">
+                  <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
+                    Phoebe Caulfield
+                  </h2>
+                  <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                  <p className="text-base">
+                    Raclette knausgaard hella meggs normcore williamsburg enamel
+                    pin sartorial venmo tbh hot chicken gentrify portland.
+                  </p>
+                </div>
+              </div>
+              <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                <p className="leading-relaxed text-lg mb-4">
+                  Meggings portland fingerstache lyft, post-ironic fixie man bun
+                  banh mi umami everyday carry hexagon locavore direct trade art
+                  party. Locavore small batch listicle gastropub farm-to-table
+                  lumbersexual salvia messenger bag. Coloring book flannel
+                  truffaut craft beer drinking vinegar sartorial, disrupt
+                  fashion axe normcore meh butcher. Portland 90's scenester
+                  vexillologist forage post-ironic asymmetrical, chartreuse
+                  disrupt butcher paleo intelligentsia pabst before they sold
+                  out four loko. 3 wolf moon brooklyn.
+                </p>
+                <a className="text-indigo-500 inline-flex items-center">
+                  Learn More
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
